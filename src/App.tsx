b39380ab460bd42e3bb01b776d23331b9fa0006d
G-Ludovic <l.galicher@free.fr>
2025-05-07 13:27:04 +0200

    App clear

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,12 @@
-// import { useState } from 'react'
-import './App.css'
+import Header from "./Components/Header/Header";
 
 function App() {
-
-
   return (
     <>
+      <Header />
+      <h1>Je suis dans le composant App</h1>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/Components/Header/Header.tsx
@@ -0,0 +1,15 @@
+import "./Header.css";
+
+function Header() {
+  return (
+    <header>
+      <nav>
+        <a href="HomePage">Home</a>
+        <a href="DrawPage">Galerie</a>
+        <a href="ContactPage">Contact</a>
+      </nav>
+    </header>
+  );
+}
+
+export default Header;
