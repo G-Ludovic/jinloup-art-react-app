6dc2ccb2e0689d03815382aa15d60c9d6bb97578
G-Ludovic <l.galicher@free.fr>
2025-05-07 15:05:46 +0200

    header css done

--- src/Components/Header/Header.tsx
+++ src/Components/Header/Header.tsx
@@ -3,6 +3,7 @@ import "./Header.css";
 function Header() {
   return (
     <header>
+      <img src="src/assets/img/logo-jinloup-ludo-art.webp" alt="logo du site" />
       <nav>
         <a href="HomePage">Home</a>
         <a href="DrawPage">Galerie</a>
