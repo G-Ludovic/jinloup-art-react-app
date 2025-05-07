51faf145419eadea0455a428a286e759df44b3fc
G-Ludovic <l.galicher@free.fr>
2025-05-07 21:03:29 +0200

    pass props v2

--- src/Components/Card/Card.tsx
+++ src/Components/Card/Card.tsx
@@ -1,10 +1,7 @@
-function Card({ specie }) {
+function Card({ specie, image }) {
   return (
     <figure>
-      <img
-        src="https://cdn.pixabay.com/photo/2024/10/30/21/50/wolf-9162574_1280.jpg"
-        alt="en cours"
-      />
+      <img src={image} alt="animaux" />
       <figcaption>{specie}</figcaption>
     </figure>
   );
--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -4,8 +4,14 @@ import "./HomePage.css";
 function HomePage() {
   return (
     <main>
-      <Card specie="Loup" />
-      <Card specie="Husky" />
+      <Card
+        specie="Loup"
+        image="https://cdn.pixabay.com/photo/2024/10/30/21/50/wolf-9162574_1280.jpg"
+      />
+      <Card
+        specie="Husky"
+        image="https://cdn.pixabay.com/photo/2024/04/13/02/18/pet-8693062_1280.jpg"
+      />
     </main>
   );
 }
