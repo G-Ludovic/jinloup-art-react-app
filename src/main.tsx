0f666522365079e1a2d593584e62343c71312dc6
G-Ludovic <l.galicher@free.fr>
2025-05-08 10:34:54 +0200

    fixbug resolved

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import "./global.css";
 import { RouterProvider } from "react-router-dom";
 import router from "./router";
 
+// biome-ignore lint/style/noNonNullAssertion: <explanation>
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <RouterProvider router={router} />
