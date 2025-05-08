1138932077bdfa492b98e2e05f09b3100e11aea8
G-Ludovic <l.galicher@free.fr>
2025-05-08 17:56:20 +0200

    update optimized work

---
+++ src/data/drawings.ts
@@ -0,0 +1,104 @@
+/** pour stocker mes images dans mon "src/assets/img..." **/
+
+/** Pour exporter mon tableau d'objets "drawings" je dois créer le fichier en ".ts"
+qui me permet d'utiliser mon fichier de déclaration de type "declarations.d.ts" **/
+
+import aono from "../assets/img/draw/Aono_by_Nakiba.jpg";
+import bird from "../assets/img/draw/Bird.jpg";
+import chara from "../assets/img/draw/Chara_Wolf_2006.jpg";
+import couple from "../assets/img/draw/Couple_loups_2024.jpg";
+import death from "../assets/img/draw/Death_Note_Concept_Samurai_2007.jpg";
+import cristou from "../assets/img/draw/DessinConcoursCristou.jpg";
+import douma from "../assets/img/draw/Douma_my_Husky.jpg";
+import dracofeu from "../assets/img/draw/Draco-et-Sala_by-Jinloup2024.JPG";
+import famille from "../assets/img/draw/FamilyHusky2004_and_IndiaChar2006.jpg";
+import flavie from "../assets/img/draw/FlavieDrawCréa.jpg";
+import fratrie from "../assets/img/draw/Fratrie_Lougaroc_by_Jinloup2024.JPG";
+import grimmjow from "../assets/img/draw/Grimmjow-Wonderwice.jpg";
+import harimaItoki from "../assets/img/draw/Harima_and_Itoko_by_Nakiba.jpg";
+import harimaYakumo from "../assets/img/draw/Harima_and_Yakumo_by_Nakiba.jpg";
+import haruka from "../assets/img/draw/Haruka_by_Nakiba.jpg";
+import itachi from "../assets/img/draw/Itachi_Uchiwa_by_Nakiba.jpg";
+import jinshi from "../assets/img/draw/Jinshi_Lycan_Guitariste.jpg";
+import kaolla from "../assets/img/draw/Kaolla_Su_by_Nakiba.jpg";
+import kei from "../assets/img/draw/Kei_Love_by_Nakiba.jpg";
+import kurogane from "../assets/img/draw/Kurogane_style_DBZ_by_Nakiba.jpg";
+import loka from "../assets/img/draw/Loka_by_Nakiba.jpg";
+import loupPoeme from "../assets/img/draw/Loup_poème_clem'.jpg";
+import loupRose from "../assets/img/draw/LoupRose_by_JinLoup-2017.jpg";
+import loveHina from "../assets/img/draw/Love_Hina_by_Nakiba.jpg";
+import lujaoka from "../assets/img/draw/Lujaoka_Jeagerjack.jpg";
+import lujaokaWolf from "../assets/img/draw/Lujaoka_Wolf_by_Nakiba.jpg";
+import megaAbsol from "../assets/img/draw/Méga-Absol_forLuv_by-Jinloup2024.JPG";
+import autoportrait from "../assets/img/draw/MonAutoportrait2008.JPG";
+import neko from "../assets/img/draw/NekoGirlNinja.jpg";
+import nyu2 from "../assets/img/draw/Nyu_Chan_2_by_Nakiba.jpg";
+import nyu from "../assets/img/draw/Nyu_Chan_by_Nakiba.jpg";
+import ookami from "../assets/img/draw/Ookami_no_Style_by_Nakiba.jpg";
+import sarah from "../assets/img/draw/Princesse_Sarah_2007.jpg";
+import renjiRukia from "../assets/img/draw/Renji_and_Rukia.jpg";
+import sakura2 from "../assets/img/draw/Sakura_Haruno_2e_by_Nakiba.jpg";
+import sakura from "../assets/img/draw/Sakura_Haruno_by_Nakiba.jpg";
+import sisu from "../assets/img/draw/Sisu_the_last_dragon_byJinloup2024.jpg";
+import soiFong from "../assets/img/draw/soi-fong.jpg";
+import testOkami from "../assets/img/draw/TestAnthrosOkami2024.jpg";
+import testHarima from "../assets/img/draw/TestCaractèreHarimaKenji2007.jpg";
+import testLycan from "../assets/img/draw/TestLycanWarriorMMORPG2008.jpg";
+import wolf from "../assets/img/draw/Wolf_by_Nakiba.jpg";
+import wolfSpirit from "../assets/img/draw/Wolf_Spirit_by_Jinloup2024.png";
+import wolfGirlAnthro from "../assets/img/draw/Wolf-Girl-Anthros-Flavie.JPG";
+import wolfGirl from "../assets/img/draw/WolfGirl_by_Jinloup_2024_final.jpg";
+import wolfMan1 from "../assets/img/draw/WolfMan_by_Jinloup_2024_1.jpg";
+import wolfMan2 from "../assets/img/draw/WolfMan_by_Jinloup_2024_final.jpg";
+import lujaokaEvo from "../assets/img/draw/Lujaoka_Wolf_evolution_by_Nakiba.jpg";
+
+export const drawings = [
+  { id: 1, name: "Aono", art: aono },
+  { id: 2, name: "Bird", art: bird },
+  { id: 3, name: "Esprits loups", art: chara },
+  { id: 4, name: "Couple de loup anthropomorphe", art: couple },
+  { id: 5, name: "Death Note style samuraï", art: death },
+  { id: 6, name: "Concour cristou", art: cristou },
+  { id: 7, name: "Ma husky bébé Douma", art: douma },
+  { id: 8, name: "Dracofeu et Salamèche", art: dracofeu },
+  { id: 9, name: "Famille husky", art: famille },
+  { id: 10, name: "Flavie", art: flavie },
+  { id: 11, name: "Fratrie Pokémon", art: fratrie },
+  { id: 12, name: "Grimmjow & Wonderwice", art: grimmjow },
+  { id: 13, name: "Harima & Itoki", art: harimaItoki },
+  { id: 14, name: "Harima & Yakumo", art: harimaYakumo },
+  { id: 15, name: "Haruka", art: haruka },
+  { id: 16, name: "Itachi", art: itachi },
+  { id: 17, name: "Jinshi Métal", art: jinshi },
+  { id: 18, name: "Kaolla", art: kaolla },
+  { id: 19, name: "Love Hina", art: kei },
+  { id: 20, name: "Kurogane", art: kurogane },
+  { id: 21, name: "Loka Guerrier", art: loka },
+  { id: 22, name: "Loup", art: loupPoeme },
+  { id: 23, name: "Loup rose", art: loupRose },
+  { id: 24, name: "Love Hina Girls", art: loveHina },
+  { id: 25, name: "Grimmjow style loup", art: lujaoka },
+  { id: 26, name: "Lujaoka Wolf", art: lujaokaWolf },
+  { id: 27, name: "Mega Absol", art: megaAbsol },
+  { id: 28, name: "Auto-portrait", art: autoportrait },
+  { id: 29, name: "Neko Mage", art: neko },
+  { id: 30, name: "Nyu-Chan 1", art: nyu2 },
+  { id: 31, name: "Nyu-Chan 2", art: nyu },
+  { id: 32, name: "Kiba Okami Style", art: ookami },
+  { id: 33, name: "Princesse Sarah", art: sarah },
+  { id: 34, name: "Renji & Rukia", art: renjiRukia },
+  { id: 35, name: "Sakura 1", art: sakura2 },
+  { id: 36, name: "Sakura 2", art: sakura },
+  { id: 37, name: "Sisu", art: sisu },
+  { id: 38, name: "SoiFong", art: soiFong },
+  { id: 39, name: "Test Okami Style", art: testOkami },
+  { id: 40, name: "Harima", art: testHarima },
+  { id: 41, name: "Test Lycans", art: testLycan },
+  { id: 42, name: "Test Loup", art: wolf },
+  { id: 43, name: "Loup posé", art: wolfSpirit },
+  { id: 44, name: "Louve Anthropomorphe", art: wolfGirlAnthro },
+  { id: 45, name: "Louve Anthropomorphe robe rouge", art: wolfGirl },
+  { id: 46, name: "Loup Anthropomorphe v.1", art: wolfMan1 },
+  { id: 47, name: "Loup Anthropomorphe v.2", art: wolfMan2 },
+  { id: 48, name: "Lujaoka Wolf Évolution", art: lujaokaEvo },
+];
